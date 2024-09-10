bebee6c769445e9f30a909d38ac95759a06208b5
Wasif Ali <148147893+wasif8090@users.noreply.github.com>
2024-09-10 17:53:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+// TypeScript can be used for more interactive features.
+// For this static example, we will just log a message.
+console.log("Resume script loaded.");
---
+++ main.ts
@@ -0,0 +1,4 @@
+// TypeScript can be used for more interactive features.
+// For this static example, we will just log a message.
+
+console.log("Resume script loaded.");
